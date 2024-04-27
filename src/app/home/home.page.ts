39c8b3372b7ebf08bf33c5774fc74b8fde989904
Your Name <you@example.com>
2024-04-27 18:26:00 +0300

    feat: add QR layout sample

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component } from '@angular/core'
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone'
+import { QRCodeModule } from 'angularx-qrcode'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, QRCodeModule],
 })
 export class HomePage {
+  sessionID = "090988"
   constructor() {}
 }
